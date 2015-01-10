7d8d27aa37857dcf867b29b4f30862e3b03a2d29
zsunlaptop <zsun@fedoraproject.org>
2015-01-11 00:59:21 +0800

    add gdanmaku to repo

---
+++ app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+from gevent.monkey import patch_all
+patch_all()
+
+import redis
+from gevent.wsgi import WSGIServer
+from flask import Flask, g
+from . import settings
+from .channel_manager import ChannelManager
+
+app = Flask(__name__)
+app.config.from_object(settings)
+redis_host=os.environ['OPENSHIFT_REDIS_HOST']
+redis_port=os.environ['OPENSHIFT_REDIS_PORT']
+redis_password=os.environ['REDIS_PASSWORD']
+r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password)
+chan_mgr = ChannelManager(app, r)
+with app.app_context():
+    chan_mgr.new_channel("demo", desc=u"演示频道, 发布、订阅均无需密码")
+
+
+@app.before_request
+def set_channel_manager():
+    g.channel_manager = chan_mgr
+    try:
+        r.ping()
+    except:
+        r.connection_pool.reset()
+    g.r = r
+
+
+from . import views
+from . import api
+from . import wechat
+
+#def main():
+app.debug = True
+ip = os.environ['OPENSHIFT_PYTHON_IP']
+port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+host_name = os.environ['OPENSHIFT_GEAR_DNS']
+http_server = WSGIServer((ip, port), app)
+print("Serving at ip:port")
+http_server.serve_forever()
+
+
+# vim: ts=4 sw=4 sts=4 expandtab
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='dm',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
+      author='zsun',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
+      install_requires=['Flask>=0.10.1'],
+	  install_requires=['redis>=2.10.1'],
+	  install_requires=['gevent>=1.0.0'],
+	  install_requires=['Werkzeug>=0.9.0'],
      )
