0159ee9645f4666416311ca11fc48a6bafb9148a
zsunlaptop <zsun@fedoraproject.org>
2015-01-11 01:05:37 +0800

    remove wrong install_requires

--- setup.py
+++ setup.py
@@ -7,8 +7,4 @@ setup(name='dm',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
-      install_requires=['Flask>=0.10.1'],
-	  install_requires=['redis>=2.10.1'],
-	  install_requires=['gevent>=1.0.0'],
-	  install_requires=['Werkzeug>=0.9.0'],
      )
